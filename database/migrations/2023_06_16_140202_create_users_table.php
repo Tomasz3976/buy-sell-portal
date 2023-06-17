f5f2dcf395958fde84d1c30f19ac9ed9bda7966d
Tomasz3976 <tomeczek2886@gmail.com>
2023-06-17 16:55:43 +0200

    feat: add user roles

--- database/migrations/2023_06_16_140202_create_users_table.php
+++ database/migrations/2023_06_16_140202_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('email')->unique();
             $table->string('phoneNumber')->unique();
+            $table->enum('role', ['user', 'admin'])->default('user');
         });
     }
 
